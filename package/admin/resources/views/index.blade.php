fcb44d92288682c3b74e1824e3a88ac35b43dd0c
Sevag Akelian <akelian.sevag@gmail.com>
2024-05-06 21:58:14 +0300

    .

--- package/admin/resources/views/index.blade.php
+++ package/admin/resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="{{asset('sevag/admin/plugins/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="/sevag/admin/public/plugins/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
     <link rel="stylesheet" href="{{asset('sevag/admin/css/app.css')}}">
 </head>
--- package/admin/routes/web.php
+++ package/admin/routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 use sevag\admin\Http\Controllers\AdminController;
 
 Route::get('/admin',[AdminController::class, 'index']);
